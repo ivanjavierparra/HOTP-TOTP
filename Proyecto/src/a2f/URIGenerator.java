978d323228545c507ca336683f9b836c2315f873
ivanjavierparra <ivanjavierparra@hotmail.com>
2019-02-01 16:20:13 -0300

    Comentarios

--- Proyecto/src/a2f/URIGenerator.java
+++ Proyecto/src/a2f/URIGenerator.java
@@ -5,6 +5,29 @@ import modelo.Configuracion;
 
 
 /**
+ *¿Qué es OATH?. OATH (Initiative for Open Authentication) es un
+ * sistema de autenticación abierto, que permite a proveedores de 
+ * soluciones robustas de autenticación convivir bajo una misma infraestructura. 
+ * Éste estándar busca compatibilidad entre distintas soluciones de seguridad y 
+ * poder integrarse con otras marcas y soluciones existentes en el mercado.
+ */
+
+/**
+ * La URI definida por OATH:
+ *                          otpauth://TYPE/LABEL?PARAMETERS
+ * TYPE: HOTP or TOTP.
+ * LABEL: se utiliza para identificar con qué cuenta está asociada una clave. Contiene un nombre de cuenta, que generalmente es un correo electrónico.
+ * PARAMETERS:
+ *          Secret: es un valor codificado en Base32. Representa la clave secreta.
+ *          Issuer: es un string que indica el proveedor o servicio al que está asociada la cuenta. 
+ *          Algorithm: éste parámetro puede tener los valores “SHA1” (por defecto), “SHA256” o “SHA512”.
+ *          Digits: éste parámetro puede tener valores {6,7,8} y determina el tamaño del código OTP. El valor predeterminado es 6.
+ *          Counter: si type es HOTP, éste parámetro establecerá el valor inicial del contador.
+ *          Period: si type es TOTP, éste parámetro define un período de tiempo para el cual un código TOTP será válido, en segundos. El valor predeterminado es 30.
+ */
+
+
+/**
  * Clase que genera una URI capaz de ser leída por la app Google Authenticator, DUO, Latch, entre otros. 
  * Ver: https://www.npmjs.com/package/otp
  *      https://github.com/google/google-authenticator/wiki/Key-Uri-Format
